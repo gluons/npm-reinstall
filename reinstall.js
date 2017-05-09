29278ed765e1380847509630d70594638b8942c9
Saran Tanpituckpong <sarunta@gmail.com>
2017-05-10 00:21:56 +0700

    :sparkles: Add pretty CLI display

--- reinstall.js
+++ reinstall.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
+
+const repo = 'https://github.com/gluons/npm-reinstall';
+
 const argv = require('yargs')
-			.usage('Usage: reinstall [options] <package> ...')
+			.usage(`Usage: ${chalk.green('reinstall')} [options] ${chalk.cyan('<package> ...')}`)
 			.help()
 			.alias('help', 'h')
 			.version()
-			.alias('version', 'v')
+			.alias('version', 'V')
 			.boolean('global')
 			.alias('global', 'g')
 			.describe('global', 'Reinstall global package')
@@ -15,14 +19,20 @@ const argv = require('yargs')
 			.boolean('save-dev')
 			.alias('save-dev', 'D')
 			.describe('save-dev', 'Reinstall package in devDependencies')
+			.boolean('verbose')
+			.alias('verbose', 'v')
+			.describe('verbose', 'Display more information')
+			.epilog(`⭐ Star me at ${repo} 🙂`)
 			.argv;
 
 const spawner = require('./lib/spawner');
 
+let verbose = argv.verbose;
+
 if (argv.global) {
-	spawner(spawner.mode.GLOBAL, argv._);
+	spawner(spawner.mode.GLOBAL, argv._, verbose);
 } else if (argv.save) {
-	spawner(spawner.mode.SAVE, argv._);
+	spawner(spawner.mode.SAVE, argv._, verbose);
 } else if (argv.saveDev) {
-	spawner(spawner.mode.SAVE_DEV, argv._);
+	spawner(spawner.mode.SAVE_DEV, argv._, verbose);
 }
